029393bd49d310a8b52e2493b5176b095f391fc6
SaJid961 <sajidhussain98@gmail.com>
2024-01-14 18:04:24 +0530

    favicon and navbar

--- portfolio/src/Navbar/navbar.js
+++ portfolio/src/Navbar/navbar.js
@@ -1,19 +1,32 @@
 import React from "react";
 import "./navbar.css";
 
+import profile from '../assets/dp2.jpg'
+
 const Navbar = () => {
+
+    const name="MD Sajid Hussain"
   return (
     <>
       <header className="container-fluid user-details">
         <div className="row p-3 ">
           <div className="col-lg-6">
-            <ul className=" d-flex  align-items-center ">
-              <li>Profile pic</li>
-              <li>Name</li>
-            </ul>
+            <div className=" d-flex   align-items-center  ">
+                <div className="profile-pic">
+                <img
+               src={profile}
+                alt="loading"
+                
+              
+                
+                />
+                </div>
+                    <p className="fw-bold">{name}</p>
+              
+            </div>
           </div>
           <div className="col-lg-6">
-            <ul className=" d-flex justify-content-around align-items-center ">
+            <ul className=" fw-bold d-flex justify-content-around align-items-center ">
               <li>Home</li>
               <li>About</li>
               <li>Projects</li>
@@ -22,6 +35,7 @@ const Navbar = () => {
           </div>
         </div>
       </header>
+      
     </>
   );
 };
